ca61eb254c7387513c8bb013dcbac02e4e41a746
Mateus Velleda Vellar <mateusvelleda@gmail.com>
2018-01-30 18:41:12 -0200

    Placeholder images set

--- src/components/CardItem/CardItem.js
+++ src/components/CardItem/CardItem.js
@@ -3,20 +3,22 @@ import React, { Component } from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
+import styles from './card-item.css';
 
 class CardItem extends Component {
     render() {
         return (
-            <Card>
+            <Card className="card-item">
                 <CardHeader
                     title={this.props.title || this.props.id}
-                    subtitle={<a href={this.props.uri} target="_blank">{this.props.uri}</a>}
+                    subtitle={<a href={this.props.uri} title={`Go to ${this.props.uri}`} target="_blank">{this.props.uri}</a>}
                     avatar={this.props.icon}
+                    className="card-item__header lp"
                 />
-                <CardMedia>
-                    <img src={this.props.image} alt={this.props.title} />
+                <CardMedia className="card-item__image">
+                    <img src={this.props.image || 'https://picsum.photos/960/540'} alt={this.props.title} />
                 </CardMedia>
-                <CardText>{this.props.description}</CardText>
+                <CardText className="card-item__description">{this.props.description}</CardText>
                 <CardActions>
                     <FlatButton label="Share"/>
                     <FlatButton label="Delete"/>
